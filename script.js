bb12e0387a86f6d73b42d41345a8ad785c5d958d
MatheusZanivan <zanivanandrade@gmail.com>
2021-06-28 13:58:11 -0300

    script implemented

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-/*alert("Bem vindo(a)!")*/
-//tudo de bom 
-//melhor lang
+alert("Bem vindo(a)!")
+const meuCabecalho = document.querySelector('h1');
+meuCabecalho.textContent = 'Ola mundo!';
+let sorvete = 'chocolate';
+if (sorvete === 'chocolate') {
+  alert('Opa, Eu amo sorvete de chocolate!');
+} else {
+  alert('Ahh, mas chocolate é o meu favorito...');
+}
+let minhaVariavel = document.querySelector('h1');
+function multiplica(num1,num2) {
+    let resultado = num1 * num2;
+    return resultado;
+  }
+  multiplica(20, 20);
+  let minhaImagem = document.querySelector('img');
+
+minhaImagem.onclick = function() {
+    let meuSrc = minhaImagem.getAttribute('src');
+    if(meuSrc === 'imagens/firefox-icon.png') {
+      minhaImagem.setAttribute ('src','imagens/firefox2.png');
+    } else {
+      minhaImagem.setAttribute ('src','imagens/firefox-icon.png');
+    }
+}

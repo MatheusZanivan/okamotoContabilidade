bfeef0a065a1a666622518d8becb81aeea09651b
MatheusZanivan <zanivanandrade@gmail.com>
2021-07-12 15:37:22 -0300

    a lot

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-// Define an object
-const obj = {counter : 0};
+//Get the button
+var mybutton = document.getElementById("topBtn");
 
-// Define Setters and Getters
-Object.defineProperty(obj, "reset", {
-  get : function () {this.counter = 0;}
-});
-Object.defineProperty(obj, "increment", {
-  get : function () {this.counter++;}
-});
-Object.defineProperty(obj, "decrement", {
-  get : function () {this.counter--;}
-});
-Object.defineProperty(obj, "add", {
-  set : function (value) {this.counter += value;}
-});
-Object.defineProperty(obj, "subtract", {
-  set : function (value) {this.counter -= value;}
-});
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
-let lastname, lastName;
-lastName = "Doe";
-lastname = "Peterson";
-document.getElementById("demo").innerHTML = lastName;
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
